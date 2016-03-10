ca7bb245c82e5952760e5359dd350698dd1c87ee
Matthew Persing <persinme@rose-hulman.edu>
2016-03-10 14:34:46 -0500

    Project setup

---
+++ test/PlusMinusLexerTest.java
@@ -0,0 +1,14 @@
+package lex;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlusMinusLexerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
