f147e11038006469580e03202a8d8f4ba3835650
Matthew Persing <persinme@rose-hulman.edu>
2016-03-10 15:14:58 -0500

    Added driver program

--- src/driver/PlusMinusDriver.java
+++ src/driver/PlusMinusDriver.java
@@ -1,9 +1,34 @@
 package driver;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import lex.ILexer;
+import lex.PlusMinusLexer;
+
 public class PlusMinusDriver {
 	
 	public static void main(String[] args) {
-		System.out.println("Testing");
+		File fileIn = new File("input_output/plusMinusLexerInput.txt");
+		BufferedReader br;
+		String inString;
+		try {
+			br = new BufferedReader(new FileReader(fileIn));
+			inString = br.readLine();
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+		ILexer lexer = new PlusMinusLexer();
+		List<String> result = lexer.lex(inString);
+		for(String s : result) {
+			System.out.println(s);
+		}
 	}
 
 }
