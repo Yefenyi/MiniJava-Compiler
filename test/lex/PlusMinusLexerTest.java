28996373cba89379982e39224e0114cd069df001
Matthew Persing <persinme@rose-hulman.edu>
2016-03-10 14:53:33 -0500

    Added test for provided case

---
+++ test/lex/PlusMinusLexerTest.java
@@ -0,0 +1,37 @@
+package lex;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlusMinusLexerTest {
+	
+	private ILexer lexer;
+	
+	@Before
+	public void setUp() {
+		this.lexer = new PlusMinusLexer();
+	}
+	
+	@Test
+	public void testTestSetup() {
+		assertTrue(lexer != null);
+	}
+	
+	@Test
+	public void testProvided() {
+		List<String> result = this.lexer.lex("123+321-+12");
+		assertTrue(result != null);
+		assertTrue(result.size() == 6);
+		assertTrue(result.get(0).equals("123"));
+		assertTrue(result.get(0).equals("plus"));
+		assertTrue(result.get(0).equals("321"));
+		assertTrue(result.get(0).equals("minus"));
+		assertTrue(result.get(0).equals("plus"));
+		assertTrue(result.get(0).equals("12"));
+	}
+
+}
