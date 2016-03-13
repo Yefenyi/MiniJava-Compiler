a6f63898ea906dc4d5f29652a73eb0ff65e1b6d0
Matthew Persing <persinme@rose-hulman.edu>
2016-03-13 15:43:29 -0400

    Added full lexer adapter

---
+++ src/lex/FullLexer.java
@@ -0,0 +1,44 @@
+package lex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.Token;
+
+public class FullLexer implements ILexer {
+	
+	private String getTypeString(int type) {
+		switch(type) {
+		case 1:
+			return "ID";
+		case 2:
+			return "Integer";
+		case 3:
+			return "Operator";
+		case 4:
+			return "Delimiter";
+		case 5:
+			return "ReservedWord";
+		case 6:
+			return "WHITESPACE SHOULDN'T BE HERE!!";
+		case 7:
+			return "COMMENT?!?!?!";
+		default:
+			return "something went terribly wrong";
+		}
+	}
+
+	@Override
+	public List<String> lex(String toLex) {
+		List<String> out = new ArrayList<String>();
+		ANTLRInputStream input = new ANTLRInputStream(toLex);
+		antlr4.PlusMinusLexer lex = new antlr4.PlusMinusLexer(input);
+		List<? extends Token> tokens = lex.getAllTokens();
+		for(Token t : tokens) {
+			out.add(this.getTypeString(t.getType()) + ", " + t.getText());
+		}
+		return out;
+	}
+
+}
