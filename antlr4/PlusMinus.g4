068e95b93dc3de9fa3cf970b63e3b51fbcedcc57
Matthew Persing <persinme@rose-hulman.edu>
2016-03-13 14:38:55 -0400

    Added antlr4 to project for lexing and parsing.
    Currently only have lexer for plusminus.

---
+++ antlr4/PlusMinus.g4
@@ -0,0 +1,9 @@
+// Testing comment
+grammar PlusMinus;
+@header {
+	package antlr4;
+}
+
+plus	: '+' ;
+minus	: '-' ;
+INT		: [0-9]+ ;
--- src/driver/PlusMinusDriver.java
+++ src/driver/PlusMinusDriver.java
@@ -6,6 +6,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.Token;
+
 import lex.ILexer;
 import lex.PlusMinusLexer;
 
@@ -24,6 +27,20 @@ public class PlusMinusDriver {
 			e.printStackTrace();
 			return;
 		}
+		ANTLRInputStream input = new ANTLRInputStream(inString);
+		antlr4.PlusMinusLexer lex = new antlr4.PlusMinusLexer(input);
+		List<? extends Token> tokens = lex.getAllTokens();
+		System.out.println("Antlr:");
+		for(Token t : tokens) {
+			if(t.getType() == 1) {
+				System.out.println("plus");
+			} else if(t.getType() == 2) {
+				System.out.println("minus");
+			} else {
+				System.out.println(t.getText());
+			}
+		}
+		System.out.println("Manual:");
 		ILexer lexer = new PlusMinusLexer();
 		List<String> result = lexer.lex(inString);
 		for(String s : result) {
