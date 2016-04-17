ca360592f35a15761adcee5cdc9067f28fa042ae
Matthew Persing <persinme@rose-hulman.edu>
2016-04-17 15:08:30 -0400

    Added skeleton for environment tracking

---
+++ src/parser/EnvironmentTracker.java
@@ -0,0 +1,14 @@
+package parser;
+
+import java.util.Map;
+import java.util.Stack;
+
+public class EnvironmentTracker {
+	
+	private Stack<Map<String,ParsedIdentifier>> environs;
+	
+	public EnvironmentTracker() {
+		
+	}
+
+}
