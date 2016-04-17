8946c9b131050e62d15a47ffae7edf41fbd9d545
Matthew Persing <persinme@rose-hulman.edu>
2016-04-17 15:24:34 -0400

    EnvironmentTracker complete

--- src/parser/EnvironmentTracker.java
+++ src/parser/EnvironmentTracker.java
@@ -8,7 +8,37 @@ public class EnvironmentTracker {
 	private Stack<Map<String,ParsedIdentifier>> environs;
 	
 	public EnvironmentTracker() {
-		
+		environs = new Stack<Map<String,ParsedIdentifier>>();
+	}
+	
+	public void addLevel(Map<String,ParsedIdentifier> newMap) {
+		environs.push(newMap);
+	}
+	
+	public void removeLevel() {
+		environs.pop();
+	}
+	
+	public void addIdentifier(ParsedIdentifier ident) {
+		environs.peek().put(ident.getName(), ident);
+	}
+	
+	public boolean identifierExists(String name) {
+		for(Map<String,ParsedIdentifier> identMap : environs) {
+			if(identMap.containsKey(name)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public String getIdentifierType(String name) {
+		for(Map<String,ParsedIdentifier> identMap : environs) {
+			if(identMap.containsKey(name)) {
+				return identMap.get(name).getType();
+			}
+		}
+		return null;
 	}
 
 }
